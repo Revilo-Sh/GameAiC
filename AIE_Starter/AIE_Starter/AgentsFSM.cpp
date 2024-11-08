fb0519bfb56c1141358f46aaaaba49900106faba
Oliver <sherwelloliver@gmail.com>
2024-11-08 16:59:48 +1030

    FSM Setup

---
+++ AIE_Starter/AIE_Starter/AgentsFSM.cpp
@@ -0,0 +1,1 @@
+#include "AgentsFSM.h"
---
+++ AIE_Starter/AIE_Starter/AgentsFSM.h
@@ -0,0 +1,6 @@
+#pragma once
+//https://aie.instructure.com/courses/1344/pages/artificial-intelligence-for-games-decision-making-1-agents-and-behaviours
+class AgentsFSM
+{
+};
+
